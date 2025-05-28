aa2b06be3419611eea7527d32a56e19d074f5357
Ajeetyadav12 <ajeetyadav25052001@gmail.com>
2025-05-28 11:04:55 +0530

    Login and Register Page is done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Products from "./components/Products";
 import Blogs from "./components/Blogs";
 import AboutUsForm from "./components/AboutUsForm";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <h1 className="text-4xl font-bold">Welcome to My App</h1>
       <p className="mt-4 text-lg">
         This is a simple React app styled with Tailwind CSS.
+        <Register></Register>
+        <Login></Login>
         <Products></Products>
         <Blogs></Blogs>
         <AboutUsForm></AboutUsForm>
